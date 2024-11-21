1a4ed6aaa75e1c90edae0d8ee6debe061eb9a5a4
Bulani <jao_1999@hotmail.com>
2024-11-20 22:33:40 -0300

    Initial commit

---
+++ src/main.js
@@ -0,0 +1,13 @@
+"use strict"
+
+import "./styles/global.css"
+import "./styles/utils.css"
+import "./styles/card.css"
+import "./styles/data.css"
+import "./styles/history.css"
+import "./styles/input.css"
+import "./styles/logo.css"
+import "./styles/progress.css"
+
+import "./modules/input.js"
+import "./modules/submit.js"
